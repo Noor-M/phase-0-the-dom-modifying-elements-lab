73d4b22d2ba250a72fb03e568cc12c0b10350ce8
Noor Mohammad <noorsaidmohammad@gmail.com>
2022-11-07 22:14:36 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const mainNode = document.getElementById('main');
+mainNode.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Noor is the champion";
+document.body.append(newHeader);
